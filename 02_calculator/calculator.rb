29e6847ae2acc7e20b3398145b8e2745d15deb6b
James Quinn <flash.jaq@gmail.com>
2017-12-13 15:31:21 +0000

    finished 02_calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,16 @@
 #write your code here
+def add(num1, num2)
+	num1+num2
+end
+
+def subtract(num1, num2)
+	num1-num2
+end
+
+def sum(arr)
+	num = 0
+	arr.each do |elem|
+		num += elem
+	end
+	num
+end
