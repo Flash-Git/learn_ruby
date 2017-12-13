b45e0669f3d4fcde30e4e6e762111e632b9180b6
James Quinn <flash.jaq@gmail.com>
2017-12-13 14:51:57 +0000

    finished 01_temperature

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,17 @@
 #write your code here
+
+def ftoc(fnum)
+	cnum = 0.0
+
+	cnum =(fnum-32)*5.0/9.0
+	puts cnum
+	cnum
+end
+
+def ctof(cnum)
+	fnum = 0.0
+
+	fnum = cnum*9.0/5.0+32
+	puts fnum
+	fnum
+end
